d9b8a7460fc6b329fb1246baa4dfdc05b2c617d8
Damian Mazur <damianmazurto@gmail.com>
2017-09-10 20:18:04 +0200

    components and routes

---
+++ src/BookShelf.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const BookShelf = () => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">Currently Reading</h2>
+            <div className="bookshelf-books">
+            <ol className="books-grid">
+                <li>
+                </li>
+            </ol>
+            </div>
+        </div>
+    )
+}
+
+export default BookShelf
---
+++ src/Library.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Library = () => {
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+              </div>
+            </div>
+        </div>
+    )
+}
+
+export default Library
